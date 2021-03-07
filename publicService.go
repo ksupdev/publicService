c3bb23e7bbce05bf69b2f87bd357a3f44aac579b
ksupdev <karoons.updev@gmail.com>
2021-03-07 21:51:00 +0700

    update service v2

--- publicService.go
+++ publicService.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func SayHi(name string) string {
     // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. My name's publicService", name)
+    message := fmt.Sprintf("Hi, %v. My name's publicService version2", name)
     return message
 }
